10176127d084fca741560914c257511fb942fbdd
Nitesh Silwal <67178303+niteshsilwal01@users.noreply.github.com>
2024-09-05 19:35:04 +0545

    Update process_image.py

--- code files/process_image.py
+++ code files/process_image.py
@@ -13,17 +13,16 @@ def capture_contour():
   
   while True:
       bool, img = cap.read()
-      if cnn_test.generate_predictions(img, actual_label="Potato") == 1:
-        imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        imgBlur = cv.GaussianBlur(imgGray, (7, 7), 1)
-        thres1 = cv.getTrackbarPos('Threshold1', 'Params')
-        thres2 = cv.getTrackbarPos('Threshold2', 'Params')
-        imgCanny = cv.Canny(imgBlur, thres1, thres2)
+      imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+      imgBlur = cv.GaussianBlur(imgGray, (7, 7), 1)
+      thres1 = cv.getTrackbarPos('Threshold1', 'Params')
+      thres2 = cv.getTrackbarPos('Threshold2', 'Params')
+      imgCanny = cv.Canny(imgBlur, thres1, thres2)
 
-        kernel = np.ones((5, 5))
-        imgDil = cv.dilate(imgCanny, kernel, iterations=1)
-        contours, _ = cv.findContours(imgDil, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-        return img, contours
+      kernel = np.ones((5, 5))
+      imgDil = cv.dilate(imgCanny, kernel, iterations=1)
+      contours, _ = cv.findContours(imgDil, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+      return [img, contours]
 
 
 
